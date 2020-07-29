b48ac21d3c89912d9f7bc593011325f1589c3667
Taylor Nyquist <tnyquist@gmail.com>
2020-07-29 13:14:28 -0500

    refactored code with page-template module

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+// // console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i+=1) {
-        console.log(profileDataArr[i]);
-    }
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-    console.log("================");
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if (err) throw err;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log("Portfolio complete! Checkout index.html to see the output!");
+});
